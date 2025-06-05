e0d618ebbc21c72853bbf6f8265fb65a756975df
Filip Verheyen <>
2025-06-05 16:54:45 +0200

    li missing default classes

--- src/MenuLinkViewExpander.php
+++ src/MenuLinkViewExpander.php
@@ -5,9 +5,9 @@ namespace Drupal\menu_link_view;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\Core\Menu\MenuLinkInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Menu\MenuLinkManagerInterface;
-use Drupal\Core\Menu\MenuLinkTreeElement;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\views\Views;
 
 /**
@@ -17,6 +17,7 @@ use Drupal\views\Views;
  * menu items generated from the view results.
  */
 class MenuLinkViewExpander {
+  use StringTranslationTrait;
 
   /**
    * The entity type manager.
@@ -47,6 +48,20 @@ class MenuLinkViewExpander {
   protected $cacheBackend;
 
   /**
+   * The logger factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
+
+  /**
+   * Debug mode flag.
+   *
+   * @var bool
+   */
+  protected $debugMode = TRUE;
+
+  /**
    * MenuLinkViewExpander constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
@@ -57,98 +72,115 @@ class MenuLinkViewExpander {
    *   The language manager.
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
    *   The cache backend.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger factory.
    */
   public function __construct(
     EntityTypeManagerInterface $entity_type_manager,
     MenuLinkManagerInterface $menu_link_manager,
     LanguageManagerInterface $language_manager,
-    CacheBackendInterface $cache_backend
+    CacheBackendInterface $cache_backend,
+    LoggerChannelFactoryInterface $logger_factory
   ) {
     $this->entityTypeManager = $entity_type_manager;
     $this->menuLinkManager = $menu_link_manager;
     $this->languageManager = $language_manager;
     $this->cacheBackend = $cache_backend;
+    $this->loggerFactory = $logger_factory;
   }
 
   /**
    * Expand view menu items.
    *
-   * @param \Drupal\Core\Menu\MenuLinkTreeElement[] $tree
-   *   The menu link tree to expand.
+   * @param array $menu_items
+   *   The menu items to expand.
+   * @param string $menu_name
+   *   The menu name.
    *
-   * @return \Drupal\Core\Menu\MenuLinkTreeElement[]
-   *   The expanded menu link tree.
+   * @return array
+   *   The expanded menu items.
    */
-  public function expandViewMenuItems(array $tree) {
-    $expanded_tree = [];
+  public function expandTreeItems(array $menu_items, string $menu_name) {
+    $expanded_items = [];
 
-    foreach ($tree as $key => $element) {
-      // Safety check - make sure we have a valid element with a link.
-      if (!($element instanceof MenuLinkTreeElement) || !$element->link) {
-        $expanded_tree[$key] = $element;
-        continue;
+    foreach ($menu_items as $key => $item) {
+      // Debug information.
+      if ($this->debugMode) {
+        $this->log('Processing menu item: @key', ['@key' => $key]);
       }
 
-      $link = $element->link;
+      // Check if this is a view menu item.
+      if ($this->isMenuLinkViewItem($item)) {
+        // This is a view menu item, get its config.
+        $view_info = $this->getViewInfoFromItem($item);
 
-      // Now we know $link is not null, so it's safe to pass to isViewMenuItem.
-      $is_view_menu_item = $this->isViewMenuItem($link);
+        if (!empty($view_info) && !empty($view_info['view_id'])) {
+          // Get the view results.
+          $view_results = $this->getViewResults($view_info['view_id'], $view_info['display_id'] ?? 'default');
 
-      if ($is_view_menu_item) {
-        // Get view items and replace this menu item with them.
-        $view_items = $this->getViewMenuItems($link);
+          if (!empty($view_results)) {
+            // Create menu items from the view results.
+            $view_menu_items = $this->createMenuItemsFromResults($view_results, $item, $menu_name);
 
-        if (!empty($view_items)) {
-          // Add the generated items to the tree.
-          foreach ($view_items as $view_item_key => $view_item) {
-            $expanded_tree[$view_item_key] = $view_item;
+            // Add the view menu items to the expanded items.
+            $expanded_items = array_merge($expanded_items, $view_menu_items);
+
+            // Skip the original item.
+            continue;
           }
         }
-        else {
-          // If no results, keep the original item as a fallback.
-          $expanded_tree[$key] = $element;
-        }
       }
-      else {
-        // For normal menu items, process their subtrees if any.
-        $expanded_tree[$key] = $element;
 
-        if ($element->hasChildren) {
-          $expanded_subtree = $this->expandViewMenuItems($element->subtree);
-          $expanded_tree[$key]->subtree = $expanded_subtree;
-        }
+      // If we get here, keep the original menu item.
+      $expanded_items[$key] = $item;
+
+      // Process children if any.
+      if (!empty($item['below'])) {
+        $expanded_items[$key]['below'] = $this->expandTreeItems($item['below'], $menu_name);
       }
     }
 
-    return $expanded_tree;
+    return $expanded_items;
   }
 
   /**
-   * Check if a menu link is a view menu item.
+   * Check if an item is a view menu item.
    *
-   * @param \Drupal\Core\Menu\MenuLinkInterface $link
-   *   The menu link to check.
+   * @param array $item
+   *   The menu item to check.
    *
    * @return bool
    *   TRUE if this is a view menu item, FALSE otherwise.
    */
-  protected function isViewMenuItem(MenuLinkInterface $link) {
+  protected function isMenuLinkViewItem(array $item) {
+    if (empty($item['original_link'])) {
+      return FALSE;
+    }
+
+    $link = $item['original_link'];
+
     // Check if this is one of our menu link view derivatives.
     if ($link->getProvider() == 'menu_link_view') {
+      if ($this->debugMode) {
+        $this->log('Found view menu item by provider: @provider', ['@provider' => $link->getProvider()]);
+      }
       return TRUE;
     }
 
     // Or check if this is a content menu link with view metadata.
     if ($link->getProvider() == 'menu_link_content') {
-      $metadata = $link->getMetaData();
       $options = $link->getOptions();
 
       // Check if our module's metadata is present.
       if (!empty($options['menu_link_view'])) {
+        if ($this->debugMode) {
+          $this->log('Found view menu item by options: @options', ['@options' => print_r($options, TRUE)]);
+        }
         return TRUE;
       }
 
       // Try to load the original entity to check.
+      $metadata = $link->getMetaData();
       if (!empty($metadata['entity_id'])) {
         try {
           $menu_link_content = $this->entityTypeManager
@@ -158,6 +190,9 @@ class MenuLinkViewExpander {
           if ($menu_link_content) {
             $options = $menu_link_content->link->options ?? [];
             if (!empty($options['menu_link_view'])) {
+              if ($this->debugMode) {
+                $this->log('Found view menu item by entity options: @options', ['@options' => print_r($options, TRUE)]);
+              }
               return TRUE;
             }
           }
@@ -173,144 +208,201 @@ class MenuLinkViewExpander {
   }
 
   /**
-   * Get menu items generated from a view.
+   * Get view information from a menu item.
    *
-   * @param \Drupal\Core\Menu\MenuLinkInterface $link
-   *   The menu link that references a view.
+   * @param array $item
+   *   The menu item.
    *
-   * @return \Drupal\Core\Menu\MenuLinkTreeElement[]
-   *   The menu items generated from the view.
+   * @return array
+   *   The view information.
    */
-  protected function getViewMenuItems(MenuLinkInterface $link) {
-    $menu_items = [];
-    $metadata = $link->getMetaData();
-    $options = $link->getOptions();
-    $view_config = $options['menu_link_view'] ?? [];
-
-    // Get view info from the menu link.
-    $view_id = $view_config['view_id'] ?? '';
-    $display_id = $view_config['display_id'] ?? 'default';
-
-    if (empty($view_id)) {
+  protected function getViewInfoFromItem(array $item) {
+    $link = $item['original_link'] ?? NULL;
+    if (!$link) {
       return [];
     }
 
-    // Try to get cached results first.
-    $cache_key = 'menu_link_view:' . $link->getPluginId();
-    $cached_data = $this->cacheBackend->get($cache_key);
+    $options = $link->getOptions();
+    $view_info = $options['menu_link_view'] ?? [];
 
-    if ($cached_data) {
-      return $cached_data->data;
+    if (empty($view_info) && $link->getProvider() == 'menu_link_content') {
+      // Try to load the original entity to get the view info.
+      $metadata = $link->getMetaData();
+      if (!empty($metadata['entity_id'])) {
+        try {
+          $menu_link_content = $this->entityTypeManager
+            ->getStorage('menu_link_content')
+            ->load($metadata['entity_id']);
+
+          if ($menu_link_content) {
+            $options = $menu_link_content->link->options ?? [];
+            $view_info = $options['menu_link_view'] ?? [];
+          }
+        }
+        catch (\Exception $e) {
+          // Unable to load the entity.
+          return [];
+        }
+      }
     }
+
+    if ($this->debugMode && !empty($view_info)) {
+      $this->log('View info: @info', ['@info' => print_r($view_info, TRUE)]);
+    }
+
+    return $view_info;
+  }
+
+  /**
+   * Get results from a view.
+   *
+   * @param string $view_id
+   *   The view ID.
+   * @param string $display_id
+   *   The display ID.
+   *
+   * @return array
+   *   The view results.
+   */
+  protected function getViewResults($view_id, $display_id = 'default') {
+    $results = [];
 
     // Load and execute the view.
     $view = Views::getView($view_id);
     if (!$view || !$view->access($display_id)) {
+      if ($this->debugMode) {
+        $this->log('Could not load view @view_id:@display_id or access denied', [
+          '@view_id' => $view_id,
+          '@display_id' => $display_id,
+        ]);
+      }
       return [];
     }
 
+    // Clear any existing arguments.
+    $view->setArguments([]);
     $view->setDisplay($display_id);
+    $view->preExecute();
     $view->execute();
 
-    // Get the results and create menu items.
+    // Get the results.
     if (!empty($view->result)) {
-      $weight = 0;
-
       foreach ($view->result as $row_index => $row) {
-        // Extract entity data from the result.
+        // If this is an entity reference field.
         if (!empty($row->_entity)) {
-          $entity = $row->_entity;
-
-          // Create menu item from the entity.
-          $menu_item = $this->createEntityMenuItem(
-            $entity,
-            $link,
-            $row_index,
-            $weight
-          );
-
-          if ($menu_item) {
-            $key = 'menu_link_view_' . $link->getPluginId() . '_' . $row_index;
-            $menu_items[$key] = $menu_item;
-            $weight++;
-          }
+          $results[] = [
+            'entity' => $row->_entity,
+            'index' => $row_index,
+          ];
         }
       }
+    }
 
-      // Cache the results.
-      $cache_tags = ['menu:' . $link->getMenuName()];
-      if ($view->storage) {
-        $cache_tags = array_merge($cache_tags, $view->storage->getCacheTags());
+    if ($this->debugMode) {
+      $this->log('View @view_id:@display_id returned @count results', [
+        '@view_id' => $view_id,
+        '@display_id' => $display_id,
+        '@count' => count($results),
+      ]);
+    }
+
+    return $results;
+  }
+
+  /**
+   * Create menu items from view results.
+   *
+   * @param array $results
+   *   The view results.
+   * @param array $parent_item
+   *   The parent menu item.
+   * @param string $menu_name
+   *   The menu name.
+   *
+   * @return array
+   *   The menu items.
+   */
+  protected function createMenuItemsFromResults(array $results, array $parent_item, string $menu_name) {
+    $menu_items = [];
+    $weight = 0;
+
+    foreach ($results as $result) {
+      $entity = $result['entity'];
+      $index = $result['index'];
+
+      if (!$entity) {
+        continue;
       }
 
-      $this->cacheBackend->set(
-        $cache_key,
-        $menu_items,
-        CacheBackendInterface::CACHE_PERMANENT,
-        $cache_tags
-      );
+      try {
+        // Generate URL and title for the entity.
+        $url = $entity->toUrl();
+        $title = $entity->label();
+
+        if (empty($title)) {
+          continue;
+        }
+
+        // Create a unique key for this menu item.
+        $key = 'menu_link_view_' . md5($parent_item['original_link']->getPluginId() . '_' . $index);
+
+        // Build the menu item.
+        $menu_items[$key] = [
+          'title' => $title,
+          'url' => $url,
+          'below' => [],
+          'original_link' => new SyntheticMenuLink([
+            'title' => $title,
+            'route_name' => $url->getRouteName(),
+            'route_parameters' => $url->getRouteParameters(),
+            'url' => $url->toString(),
+            'menu_name' => $menu_name,
+            'parent' => $parent_item['original_link']->getPluginId(),
+            'weight' => $weight,
+            'expanded' => FALSE,
+            'provider' => 'menu_link_view',
+            'metadata' => [
+              'entity_id' => $entity->id(),
+              'entity_type' => $entity->getEntityTypeId(),
+              'view_row_index' => $index,
+              'parent_link_id' => $parent_item['original_link']->getPluginId(),
+            ],
+          ]),
+          'in_active_trail' => $parent_item['in_active_trail'] ?? FALSE,
+          'attributes' => [
+            'class' => [
+              'menu-item',
+              'menu-link-view-generated',
+            ],
+          ],
+        ];
+
+        $weight++;
+      }
+      catch (\Exception $e) {
+        if ($this->debugMode) {
+          $this->log('Error creating menu item from entity: @message', ['@message' => $e->getMessage()]);
+        }
+      }
+    }
+
+    if ($this->debugMode) {
+      $this->log('Created @count menu items from view results', ['@count' => count($menu_items)]);
     }
 
     return $menu_items;
   }
 
   /**
-   * Create a menu item from an entity.
+   * Log a message if debug mode is on.
    *
-   * @param object $entity
-   *   The entity to create a menu item for.
-   * @param \Drupal\Core\Menu\MenuLinkInterface $parent_link
-   *   The parent menu link.
-   * @param int $row_index
-   *   The row index in the view results.
-   * @param int $weight
-   *   The weight of the menu item.
-   *
-   * @return \Drupal\Core\Menu\MenuLinkTreeElement|null
-   *   The menu link tree element, or null if it could not be created.
+   * @param string $message
+   *   The message to log.
+   * @param array $context
+   *   The context for the message.
    */
-  protected function createEntityMenuItem($entity, MenuLinkInterface $parent_link, $row_index, $weight) {
-    try {
-      // Generate a URL for the entity.
-      $url = $entity->toUrl();
-
-      // Create a dynamic menu link definition.
-      $menu_link_definition = [
-        'title' => $entity->label(),
-        'route_name' => $url->getRouteName(),
-        'route_parameters' => $url->getRouteParameters(),
-        'url' => $url->toString(),
-        'menu_name' => $parent_link->getMenuName(),
-        'parent' => $parent_link->getPluginId(),
-        'weight' => $weight,
-        'expanded' => FALSE,
-        'provider' => 'menu_link_view',
-        'metadata' => [
-          'entity_id' => $entity->id(),
-          'entity_type' => $entity->getEntityTypeId(),
-          'view_row_index' => $row_index,
-          'parent_link_id' => $parent_link->getPluginId(),
-        ],
-      ];
-
-      // Create a synthetic menu link.
-      $synthetic_menu_link = new SyntheticMenuLink($menu_link_definition);
-
-      // Create a tree element for this menu link.
-      $tree_element = new MenuLinkTreeElement(
-        $synthetic_menu_link,
-        FALSE,
-        0,
-        FALSE,
-        []
-      );
-
-      return $tree_element;
-    }
-    catch (\Exception $e) {
-      // If we can't create a menu item from this entity, return null.
-      return NULL;
-    }
+  protected function log($message, array $context = []) {
+    $this->loggerFactory->get('menu_link_view')->notice($message, $context);
   }
 
 }
--- src/SyntheticMenuLink.php
+++ src/SyntheticMenuLink.php
@@ -32,120 +32,232 @@ class SyntheticMenuLink implements MenuLinkInterface {
   /**
    * {@inheritdoc}
    */
-  public function getTitle() {
+  public function getTitle(): string {
     return $this->definition['title'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getRouteName() {
+  public function getDescription(): string {
+    return $this->definition['description'] ?? '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRouteName(): string {
     return $this->definition['route_name'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getRouteParameters() {
+  public function getRouteParameters(): array {
     return $this->definition['route_parameters'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getUrlObject() {
+  public function getUrlObject($title_attribute = TRUE): Url {
     return Url::fromRoute($this->getRouteName(), $this->getRouteParameters());
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getWeight() {
+  public function getWeight(): int {
     return $this->definition['weight'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getPluginId() {
+  public function getPluginId(): string {
     return 'menu_link_view_synthetic_' . $this->definition['metadata']['entity_id'] . '_' . $this->definition['metadata']['view_row_index'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getMenuName() {
+  public function getMenuName(): string {
     return $this->definition['menu_name'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getParent() {
+  public function getParent(): string {
     return $this->definition['parent'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function isExpanded() {
+  public function isExpanded(): bool {
     return $this->definition['expanded'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getProvider() {
+  public function isTranslatable(): bool {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getProvider(): string {
     return $this->definition['provider'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getMetaData() {
+  public function getMetaData(): array {
     return $this->definition['metadata'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getOptions() {
+  public function getOptions(): array {
+    return $this->definition['options'] ?? [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEnabled(): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isResettable(): bool {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function updateLink(array $new_definition_values, $persist): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isDeletable(): bool {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function deleteLink(): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormClass(): string {
+    return '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDeleteRoute(): ?Url {
+    return NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEditRoute(): ?Url {
+    return NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTranslateRoute(): ?Url {
+    return NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getResetRoute(): ?Url {
+    return NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeId(): ?string {
+    return NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBaseId(): string {
+    return 'menu_link_view_synthetic';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPluginDefinition(): array {
+    return $this->definition;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getNoTranslate(): ?bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLocalizationOptions(): array {
     return [];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function isEnabled() {
-    return TRUE;
+  public function getOperations(): array {
+    return [];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function isResettable() {
-    return FALSE;
+  public function getCacheContexts(): array {
+    return [];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function updateLink(array $new_definition_values, $persist) {
-    return TRUE;
+  public function getCacheTags(): array {
+    return [];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function isDeletable() {
-    return FALSE;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function deleteLink() {
-    return TRUE;
+  public function getCacheMaxAge(): int {
+    return 0;
   }
 
 }
